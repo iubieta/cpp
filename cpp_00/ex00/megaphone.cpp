4a990f68fecd9a560e9a14efdd631bcef91a6802
iubieta <ikerp.ubieta@gmail.com>
2025-02-11 23:49:47 +0100

    Tuesday 2025-02-11 23:49:47

---
+++ cpp_00/ex00/megaphone.cpp
@@ -0,0 +1,48 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   megaphone.cpp                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: iubieta- <iubieta@student.42.fr>           +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/02/11 23:09:00 by iubieta-          #+#    #+#             */
+/*   Updated: 2025/02/11 23:32:26 by iubieta-         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+// Ejemplo de funcionamiento:
+// $>./megaphone "shhhhh... I think the students are asleep..."
+// SHHHHH... I THINK THE STUDENTS ARE ASLEEP...
+// $>./megaphone Damnit " ! " "Sorry students, I thought this thing was off."
+// DAMNIT ! SORRY STUDENTS, I THOUGHT THIS THING WAS OFF.
+// $>./megaphone
+// * LOUD AND UNBEARABLE FEEDBACK NOISE *
+
+#include <iostream>
+#include <cctype>
+
+std::string trim(const std::string& str) {
+    size_t first = str.find_first_not_of(" \t\n\r\f\v");
+    if (first == std::string::npos) return "";
+
+    size_t last = str.find_last_not_of(" \t\n\r\f\v");
+    return str.substr(first, (last - first + 1));
+}
+
+int	main(int argc, char **argv) {
+	// Caso sin argumentos
+	if (argc == 1) {
+		std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *";
+		return 0;
+	}
+
+	for (int i = 1; i < argc; i++) {
+		std::string str = argv[i];
+		str = trim(str);
+		for (size_t j = 0; j < str.length(); j++) {
+			std::cout << (char)std::toupper(str[j]);
+		}
+		std::cout << " ";
+	}
+	std::cout << '\n';
+}
---
+++ cpp_00/notas.md
@@ -0,0 +1,27 @@
+### Compilacion
+- c++
+- flags : -Wall -Wextra - Werror (-std=c++98)
+
+### Formato
+- Class names in UpperCamelCase
+- Archivo que contengan clases : NombreDeLaClase.extension
+- Mensajes de salida seguidos de un salto de linea siempre
+- NO HAY NORMINETTE!
+
+### Funciones permitidas y prohibidas
+- Cualquier funcion de la libreria estandar esta permitida
+- Cualquier otra funcion o libreria esta prohibida
+- Tambien esta prohibido :
+    - printf()
+    - alloc()
+    - free()
+- Prohibido el uso de `using namespace <>` y `friend`
+- Prohibido los `Containers` y `Algorithms` excepto en el modulo 08 y 09
+
+### Requisitos
+- Evitar leaks de memoria
+- Clases diseñadas en `Orthodox Canonical Form` excepto si se indica lo contrario
+- Prohibidas las funciones en archivos de cabecera
+- Los archivos de cabecera deben ser independientes y autonomos
+- Se debe evitar la doble inclusion con `include guards`
+
