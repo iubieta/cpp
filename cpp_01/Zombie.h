77ed6c41a4b24c7a4daf16e5edac7067d94ae373
iubieta <ikerp.ubieta@gmail.com>
2025-06-22 21:00:48 +0200

    CPP_01: ex00

---
+++ cpp_01/Zombie.h
@@ -0,0 +1,32 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Zombie.h                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: iubieta- <iubieta@student.42.fr>           +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/06/22 19:37:03 by iubieta-          #+#    #+#             */
+/*   Updated: 2025/06/22 20:06:15 by iubieta-         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef ZOMBIE_H
+#define ZOMBIE_H 
+
+#include <iostream>
+#include <string>
+
+class Zombie {
+	private:
+		std::string	name;
+
+	public:
+		Zombie(std::string _name);
+		
+		void announce(void);
+};
+
+Zombie *newZombie(std::string name);
+void randomChump(std::string name);
+
+#endif
