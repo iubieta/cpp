129abb75571c4dc6f3b82d9ce587e82f5e4f9e37
Iker Ubieta Perez <iubieta-@c3r4s2.42urduliz.com>
2026-02-17 19:28:36 +0100

    wip (cpp08/ex02)

---
+++ cpp_08/ex02/MutantStack.hpp
@@ -0,0 +1,32 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   MutantStack.hpp                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: iubieta- <iubieta@student.42.fr>           +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/02/17 18:29:22 by iubieta-          #+#    #+#             */
+/*   Updated: 2026/02/17 19:25:30 by iubieta-         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stack>
+
+template <typename T>
+class MutantStack : public std::stack<T> {
+	public:
+		MutantStack() {};
+		MutantStack(const MutantStack<T>& other) : std::stack<T>(other) {}
+		MutantStack& operator=(const MutantStack& other) {
+			if (this != &other)
+				std::stack<T>::operator=(other);
+			return *this;
+		}
+		~MutantStack() {}
+
+		typedef typename std::stack<T>::container_type::iterator iterator;
+		iterator begin() { return this->c.begin(); }
+		iterator end() { return this->c.end(); }
+		const iterator begin() const { return this->c.begin(); }
+		const iterator end() const { return this->c.end(); }
+};
