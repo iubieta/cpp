b8ebb8d473a2827d960711253ea2154f06c84102
Iker Ubieta <ikerp.ubieta@gmail.com>
2026-02-22 20:09:24 +0100

    fix (cpp08/ex02): const iterator

--- cpp_08/ex02/MutantStack.hpp
+++ cpp_08/ex02/MutantStack.hpp
@@ -27,6 +27,7 @@ class MutantStack : public std::stack<T> {
 		typedef typename std::stack<T>::container_type::iterator iterator;
 		iterator begin() { return this->c.begin(); }
 		iterator end() { return this->c.end(); }
-		const iterator begin() const { return this->c.begin(); }
-		const iterator end() const { return this->c.end(); }
+		typedef typename std::stack<T>::container_type::const_iterator const_iterator;
+		const_iterator begin() const { return this->c.begin(); }
+		const_iterator end() const { return this->c.end(); }
 };
