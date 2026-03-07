4d98648d011a81858c05c914801e908b169d4497
Iker Ubieta <ikerp.ubieta@gmail.com>
2026-03-07 14:34:52 +0100

    test (cpp09/ex01)

--- cpp_09/ex01/main.cpp
+++ cpp_09/ex01/main.cpp
@@ -12,10 +12,52 @@
 
 #include "RPN.hpp"
 #include <iostream>
+#include <string>
 
 int main(int argc, char *argv[]) {
-	if (argc != 2)
+	if (argc != 2) {
 		std::cout << "ERROR: check your arguments" << std::endl;
-	RPN rpn((std::string(argv[1])));
-	std::cout << rpn.evaluate() << std::endl;
+		return 1;
+	}
+	std::string input(argv[1]);
+		if (input == "test") {
+			{
+				RPN rpn("8 9 * 9 - 9 - 9 - 4 - 1 +");
+				std::cout << rpn.evaluate() << std::endl;
+			}
+			{
+				RPN rpn("7 7 * 7 -");
+				std::cout << rpn.evaluate() << std::endl;
+			}
+			{
+				RPN rpn("1 2 * 2 / 2 * 2 4 - +");
+				std::cout << rpn.evaluate() << std::endl;
+			}
+			try {
+				RPN rpn("(1 + 1)"); // Invalid token
+				std::cout << rpn.evaluate() << std::endl;
+			} catch (std::exception &e) {
+				std::cout << e.what() << std::endl;
+			}
+			try {
+				RPN rpn("1 2 3 +"); // Incomplete expression
+				std::cout << rpn.evaluate() << std::endl;
+			} catch (std::exception &e) {
+				std::cout << e.what() << std::endl;
+			}
+			try {
+				RPN rpn("1 0 /"); // Division by zero
+				std::cout << rpn.evaluate() << std::endl;
+			} catch (std::exception &e) {
+				std::cout << e.what() << std::endl;
+			}
+		} else {
+			try {
+				RPN rpn(input);
+				std::cout << rpn.evaluate() << std::endl;
+			} catch (std::exception &e) {
+				std::cout << e.what() << std::endl;
+			}
+		}
+	return 0;
 }
